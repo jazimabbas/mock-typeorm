aade916977095f97bf28fd523d0d027c5979cdc6
Jazim Abbas <jazimabbas87@gmail.com>
2024-06-15 13:51:07 +0500

    🚀 Initial Setup

---
+++ src/type/typeorm.ts
@@ -0,0 +1,30 @@
+import { Repository, SelectQueryBuilder } from "typeorm";
+
+type SelectQueryBuilderMethodObj = {
+  [K in keyof SelectQueryBuilder<any> as SelectQueryBuilder<any>[K] extends (...args: any[]) => any
+    ? K
+    : never]: SelectQueryBuilder<any>[K];
+};
+
+type FilterMethods<T> = {
+  [K in keyof SelectQueryBuilderMethodObj as SelectQueryBuilderMethodObj[K] extends (
+    ...args: any[]
+  ) => T
+    ? K
+    : never]: SelectQueryBuilderMethodObj[K];
+};
+
+type ExtractMethodNames<T> = keyof T;
+
+export type SelfReferenceQueryBuilderMethods = Exclude<
+  ExtractMethodNames<FilterMethods<SelectQueryBuilder<any>>>,
+  "createQueryBuilder"
+>;
+
+export type QueryBuilderReturnMethods = ExtractMethodNames<FilterMethods<Promise<any>>>;
+
+type AllRepositoryMethods = {
+  [K in keyof Repository<any>]: K;
+}[keyof Repository<any>];
+
+export type RepositoryMethods = Exclude<AllRepositoryMethods, "createQueryBuilder">;
